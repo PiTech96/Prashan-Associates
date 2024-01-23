3ff626fa1127b837c8a8bee263d320f8fe26cb3d
sushant <pitechology69@gmail.com>
2024-01-23 21:42:34 +0545

    feat: homepage about added

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,4 +1,5 @@
 
+import HomeAboutUs from './aboutus'
 import HeroSection from './hero'
 import HomeServices from './homeServices'
 
@@ -7,6 +8,7 @@ function Home() {
     <div>
       <HeroSection/>
       <HomeServices/>
+      <HomeAboutUs/>
     </div>
   )
 }
